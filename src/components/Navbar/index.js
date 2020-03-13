851b2683f9e50192664acd0d2b0d6e9d0fd68a29
adembacaj <adibacaj@live.com>
2020-03-13 14:17:36 +0100

    a bug fixed at navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import './navbar.scss';
 
 function Navbar(props) {
-    const onClick = useCallback((e) => {props.history.push(`${e.target.name}`)}, [])
+    const [isActive, setIsActive] = useState('/')
+    const onClick = useCallback((e) => {
+        props.history.push(`${e.target.name}`)
+        setIsActive(e.target.name)
+    }, [])
     
     return (
         <div className="navbar">
@@ -11,13 +15,13 @@ function Navbar(props) {
             </div>
             <div className="navbar__right">
                 <ul>
-                    <li><button name="/" onClick={onClick}>Home</button></li>
-                    <li><button name="/products" onClick={onClick}>Products</button></li>
-                    <li><button name="/services" onClick={onClick}>Services</button></li>
-                    <li><button name="/shops" onClick={onClick}>Shops</button></li>
-                    <li><button name="/shop-assistant" onClick={onClick}>Assistants</button></li>
-                    <li><button name="/customers" onClick={onClick}>Customers</button></li>
-                    <li><button name="/orders" onClick={onClick}>Orders</button></li>
+                    <li><button is-active={isActive === '/' ? 'true': 'false'} name="/" onClick={onClick}>Home</button></li>
+                    <li><button is-active={isActive === '/products' ? 'true': 'false'} name="/products" onClick={onClick}>Products</button></li>
+                    <li><button is-active={isActive === '/services' ? 'true': 'false'} name="/services" onClick={onClick}>Services</button></li>
+                    <li><button is-active={isActive === '/shops' ? 'true': 'false'} name="/shops" onClick={onClick}>Shops</button></li>
+                    <li><button is-active={isActive === '/shop-assistant' ? 'true': 'false'} name="/shop-assistant" onClick={onClick}>Assistants</button></li>
+                    <li><button is-active={isActive === '/customers' ? 'true': 'false'} name="/customers" onClick={onClick}>Customers</button></li>
+                    <li><button is-active={isActive === '/orders' ? 'true': 'false'} name="/orders" onClick={onClick}>Orders</button></li>
                 </ul>
             </div>
         </div>
