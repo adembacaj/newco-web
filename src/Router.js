c875db6b22a6a19d52f08258222b82e2a67c7e74
adembacaj <adibacaj@live.com>
2020-03-13 15:37:01 +0100

    a bug fixed with suspense

--- src/Router.js
+++ src/Router.js
@@ -1,50 +1,51 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Router as BaseRouter, Route, Switch } from 'react-router-dom';
 import history from './history';
+import Navbar from './components/Navbar';
+import PageLoader from './components/PageLoader';
 
 //pages
-import Home from './pages/Home';
-import Products from './pages/Products';
-import ProductsForm from './pages/Products/productsForm';
-import Services from './pages/Services';
-import ServicesForm from './pages/Services/servicesForm';
-import Shops from './pages/Shops';
-import ShopsForm from './pages/Shops/shopsForm';
-import Orders from './pages/Orders';
-import OrdersForm from './pages/Orders/OrdersForm';
-import ShopAssistants from './pages/ShopAssistants';
-import ShopAssistantsForm from './pages/ShopAssistants/shopAssistantsForm';
-import Customers from './pages/Customers';
-import CustomersForm from './pages/Customers/customersForm';
-
-//components
-import Navbar from './components/Navbar';
+const Home = lazy(() => import('./pages/Home'));
+const Products = lazy(() => import('./pages/Products'));
+const ProductsForm = lazy(() => import('./pages/Products/productsForm'));
+const Services = lazy(() => import('./pages/Services'));
+const ServicesForm = lazy(() => import('./pages/Services/servicesForm'));
+const Shops = lazy(() => import('./pages/Shops'));
+const ShopsForm = lazy(() => import('./pages/Shops/shopsForm'));
+const Orders = lazy(() => import('./pages/Orders'));
+const OrdersForm = lazy(() => import('./pages/Orders/OrdersForm'));
+const ShopAssistants = lazy(() => import('./pages/ShopAssistants'));
+const ShopAssistantsForm = lazy(() => import('./pages/ShopAssistants/shopAssistantsForm'));
+const Customers = lazy(() => import('./pages/Customers'));
+const CustomersForm = lazy(() => import('./pages/Customers/customersForm'));
 
 function Router(props) {
     return (
         <BaseRouter history={history}>
             <Navbar history={history} />
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path="/products-form/:productId" component={ProductsForm} />
-                <Route path='/products-form' component={ProductsForm} />
-                <Route path='/products' component={Products} />
-                <Route path="/services-form/:serviceId" component={ServicesForm} />
-                <Route path='/services-form' component={ServicesForm} />
-                <Route path='/services' component={Services} />
-                <Route path="/shops-form/:shopId" component={ShopsForm} />
-                <Route path='/shops-form' component={ShopsForm} />
-                <Route path='/shops' component={Shops} />
-                <Route path="/orders-form/:orderId" component={OrdersForm} />
-                <Route path='/orders-form' component={OrdersForm} />
-                <Route path='/orders' component={Orders} />
-                <Route path="/assistants-form/:assistantId" component={ShopAssistantsForm} />
-                <Route path='/assistants-form' component={ShopAssistantsForm} />
-                <Route path='/shop-assistant' component={ShopAssistants} />
-                <Route path="/customers-form/:customerId" component={CustomersForm} />
-                <Route path='/customers-form' component={CustomersForm} />
-                <Route path='/customers' component={Customers} />
-            </Switch>
+            <Suspense fallback={<PageLoader />}>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path="/products-form/:productId" component={ProductsForm} />
+                    <Route path='/products-form' component={ProductsForm} />
+                    <Route path='/products' component={Products} />
+                    <Route path="/services-form/:serviceId" component={ServicesForm} />
+                    <Route path='/services-form' component={ServicesForm} />
+                    <Route path='/services' component={Services} />
+                    <Route path="/shops-form/:shopId" component={ShopsForm} />
+                    <Route path='/shops-form' component={ShopsForm} />
+                    <Route path='/shops' component={Shops} />
+                    <Route path="/orders-form/:orderId" component={OrdersForm} />
+                    <Route path='/orders-form' component={OrdersForm} />
+                    <Route path='/orders' component={Orders} />
+                    <Route path="/assistants-form/:assistantId" component={ShopAssistantsForm} />
+                    <Route path='/assistants-form' component={ShopAssistantsForm} />
+                    <Route path='/shop-assistant' component={ShopAssistants} />
+                    <Route path="/customers-form/:customerId" component={CustomersForm} />
+                    <Route path='/customers-form' component={CustomersForm} />
+                    <Route path='/customers' component={Customers} />
+                </Switch>
+            </Suspense>
         </BaseRouter>
     )
 }
